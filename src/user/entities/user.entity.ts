819e6470fe76aac5ef31f2c342ec180ce11ffd91
ashafa3331 <ashafa3331@gmail.com>
2022-11-23 15:14:04 +0200

    nestjs ecommerce start up project

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import { Exclude } from "class-transformer";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn("uuid")
+   id: string
+
+   @Column({unique:true})
+   username: string
+
+   @Column({unique:true})
+   email: string
+
+   @Column()
+    @Exclude()
+   password: string
+
+   @Column()
+   role: string
+
+   @CreateDateColumn()
+   createdAt : String
+
+   @UpdateDateColumn()
+   updtedAt : String
+   
+
+//    @OneToMany(type => CartEntity, cart => cart.id)
+//    @JoinColumn()
+//    cart: CartEntity[]
+
+//    @OneToOne(type => OrderEntity, order => order.id)
+//    @JoinColumn()
+//    order : OrderEntity;
+}
